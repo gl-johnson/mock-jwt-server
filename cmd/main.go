ca9279b802b83ef2b085f66d76d851a413e7dee6
Glen Johnson <glen.johnson@cyberark.com>
2024-08-02 11:07:54 -0600

    Implementation

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gl-johnson/mock-jwt-server/pkg/handlers"
+)
+
+func main() {
+	http.HandleFunc("/.well-known/jwks.json", handlers.JWKSHandler)
+	http.HandleFunc("/jwks", handlers.JWKSHandler)
+	http.HandleFunc("/token", handlers.TokenHandler)
+	http.HandleFunc("/", handlers.DynamicKeyHandler)
+
+	fmt.Printf(`
+mock-jwt-server usage:
+
+GET /token - Issue token with default key/alg (RS256)
+GET /.well-known/jwks.json - Get complete JWKS
+GET /<key_id>/<algorithm> - Get/create specified JWKS
+POST /<key_id>/<algorithm> - Sign token with specified key/alg
+DELETE /<key_id> - Delete key
+
+Listening on :8080. Mapped to port %s on the host.
+`, os.Getenv("EXTERNAL_PORT"))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/cyberark/mock-jwt-server
+
+go 1.21.5
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	gopkg.in/square/go-jose.v2 v2.6.0
+)
+
+require (
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+)
